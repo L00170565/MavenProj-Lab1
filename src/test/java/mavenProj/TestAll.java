922a7231cdf535a88742eb3a2cad2d51553d107f
L00170565 <L00170565@student.lyit.ie>
2021-11-19 03:37:24 -0800

    first commit

---
+++ src/test/java/mavenProj/TestAll.java
@@ -0,0 +1,13 @@
+
+package mavenProj;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({CalculatorTest.class,AppTest.class})
+
+public class TestAll {
+
+}
